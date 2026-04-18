0568f9ca710f1ee00290cb44d6aa53bbd6c52fbd
tachpad <vkchempel@gmail.com>
2026-04-18 19:48:52 +0100

    Req 1: Final UI adjustments and search functionality

---
+++ src/main/java/com/example/vitaliipetitions/VitaliipetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.vitaliipetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaliipetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaliipetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vitaliipetitions/VitaliipetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitaliipetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaliipetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
